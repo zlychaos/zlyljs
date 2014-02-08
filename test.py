6cfb737a5cfac3c00f8c4711f548e75225858917
Liyuan Zheng <eric@dyn-160-39-196-236.dyn.columbia.edu>
2014-02-08 16:51:44 -0500

    structured

--- test.py
+++ test.py
@@ -1,26 +1,45 @@
 import urllib2
 import base64
 import json
+import sys
 
-bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27gates%27&$top=10&$format=json'
 #Provide your account key here
 accountKey = 'wzQz8YO7jqhGSx1UpWwYRiVwlb3KuGOxavRpambmZY8'
 
 accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
 headers = {'Authorization': 'Basic ' + accountKeyEnc}
-req = urllib2.Request(bingUrl, headers = headers)
-response = urllib2.urlopen(req)
-content = response.read()
-#content contains the xml/json response from Bing. 
-#print content
-json_content = json.loads(content)
-result = json_content["d"]["results"]
-i = 0
-for item in result:
-	i = i+1
-	print "--------------------"
-	print i
-	print "--------------------"
-	
-	print item
-  
+
+def getResult(query):
+	bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27' + query + '&$top=10&$format=json'
+	req = urllib2.Request(bingUrl, headers = headers)
+	response = urllib2.urlopen(req)
+	content = response.read()
+	#content contains the xml/json response from Bing. 
+	#print content
+	json_content = json.loads(content)
+	result = json_content["d"]["results"]
+	return result
+
+def display(result):
+	i = 0
+	for item in result:
+        	i = i+1
+        	print "--------------------"
+        	print i
+        	print "--------------------"
+        	print "Title:  "+item[u'Title']
+        	print "Description:"
+        	print item[u'Description']
+		print item[u'Url']
+
+
+line = sys.stdin.readline()
+while line:
+	words = line.split()
+	query = ''
+	for word in words:
+		query = query + word + '%27'
+	result = getResult(query)
+	display(result)
+	line = sys.stdin.readline()
+
