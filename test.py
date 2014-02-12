3ca3c4fa18ee82cf893bfdf978c11dcfa355dd9a
Liyuan Zheng <lz2375@columbia.edu>
2014-02-12 14:23:47 -0500

    minor code-style improvement

--- test.py
+++ test.py
@@ -10,36 +10,37 @@ accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
 headers = {'Authorization': 'Basic ' + accountKeyEnc}
 
 def getResult(query):
-	bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27' + query + '&$top=10&$format=json'
-	req = urllib2.Request(bingUrl, headers = headers)
-	response = urllib2.urlopen(req)
-	content = response.read()
-	#content contains the xml/json response from Bing. 
-	#print content
-	json_content = json.loads(content)
-	result = json_content["d"]["results"]
-	return result
+    bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27' + query + '&$top=10&$format=json'
+    req = urllib2.Request(bingUrl, headers = headers)
+    response = urllib2.urlopen(req)
+    content = response.read()
+    #content contains the xml/json response from Bing. 
+    #print content
+    json_content = json.loads(content)
+    result = json_content["d"]["results"]
+    return result
 
 def display(result):
-	i = 0
-	for item in result:
-        	i = i+1
-        	print "--------------------"
-        	print i
-        	print "--------------------"
-        	print "Title:  "+item[u'Title']
-        	print "Description:"
-        	print item[u'Description']
-		print item[u'Url']
+    i = 0
+    for item in result:
+            i = i+1
+            print "--------------------"
+            print i
+            print "--------------------"
+            print "Title:  "+item[u'Title']
+            print "Description:"
+            print item[u'Description']
+        print item[u'Url']
 
 
-line = sys.stdin.readline()
-while line:
-	words = line.split()
-	query = ''
-	for word in words:
-		query = query + word + '%27'
-	result = getResult(query)
-	display(result)
-	line = sys.stdin.readline()
+if __name__ =="__main__":
+    line = sys.stdin.readline()
+    while line:
+        words = line.split()
+        query = ''
+        for word in words:
+            query = query + word + '%27'
+        result = getResult(query)
+        display(result)
+        line = sys.stdin.readline()
 
