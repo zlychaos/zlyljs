4da6c7eeb1fb998d576f83d581f0a12399dfaf86
Liyuan Zheng <lz2375@columbia.edu>
2014-02-16 17:46:34 -0500

    drop debug printing

--- keyword_order.py
+++ keyword_order.py
@@ -27,7 +27,7 @@ def set_order(keywords, results):
                             matrix[word1][word2] = matrix[word1][word2] - 1
     best_query = keywords
     score = 0
-    print keywords
+    #print keywords
     for word1 in keywords:
         words_list = keywords[:]
         query = [word1]
@@ -40,15 +40,15 @@ def set_order(keywords, results):
             for word2 in words_list:
                 if word2 not in query and ( word_to_add==None or ( matrix[curr_word].has_key(word2) and  matrix[curr_word][word2]>matrix[curr_word][word_to_add] ) ):
                     word_to_add = word2
-            print word_to_add
+            #print word_to_add
             query.append(word_to_add)
             words_list.remove(word_to_add)
             curr_word = word_to_add
         the_score = 0
         for i in range(len(query)-1):
-            print i
+            #print i
             the_score = the_score + matrix[query[i]][query[i+1]]
-        print the_score
+        #print the_score
         if the_score > score:
             best_query = query
             score = the_score
